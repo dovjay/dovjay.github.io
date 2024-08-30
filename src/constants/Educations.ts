2e947c8e63bc361142f271ad7866811f591a30c5
Dovan Sanjaya <dovansanjaya24@gmail.com>
2024-08-30 10:29:02 +0700

    basic home page layout

--- src/constants/Educations.ts
+++ src/constants/Educations.ts
@@ -2,7 +2,7 @@ export const Educations = [
   {
     school: "SMA N 1 Way Jepara",
     duration: "2015 - 2018",
-    degree: "IPA",
+    degree: "Highschool, Science",
   },
 ];
 
@@ -12,17 +12,6 @@ export const Certificates = [
     institution: "Hacktiv8 Indonesia",
     issued: "Dec 2018",
     credential: "",
-  },
-  {
-    name: "Fullstack Javascript Immersive Program",
-    institution: "Hacktiv8 Indonesia",
-    issued: "Dec 2018",
-    credential: "",
-  },
-  {
-    name: "Fullstack Javascript Immersive Program",
-    institution: "Hacktiv8 Indonesia",
-    issued: "Dec 2018",
-    credential: "",
+    skills: ["React", "Tailwind", "Next.js", "Figma"],
   },
 ]
