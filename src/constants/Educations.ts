682749f89038ac5cc5d62122808c69b5ed44a654
Dovan Sanjaya <dovansanjaya24@gmail.com>
2024-09-03 10:36:59 +0700

    finishing about, exp, ed section

--- src/constants/Educations.ts
+++ src/constants/Educations.ts
@@ -8,10 +8,43 @@ export const Educations = [
 
 export const Certificates = [
   {
+    name: "Frontend Development Libraries",
+    institution: "freeCodeCamp",
+    logo: "/images/education/freecodecamp.png",
+    issued: "Jun 2022",
+    credential: "https://www.freecodecamp.org/certification/dovjay/front-end-development-libraries",
+    skills: ["Bootstrap", "jQuery", "SASS", "React", "Redux"],
+  },
+  {
+    name: "Unity Mobile C# Developer Course",
+    institution: "GameDev.tv",
+    logo: "/images/education/gamedevtv.jpg",
+    issued: "Jun 2022",
+    credential: "https://drive.google.com/file/d/1OFmBCQeBDq0CxuviiRTW7C5vKUj71uV2/view?usp=sharing",
+    skills: ["C#", "Unity"],
+  },
+  {
     name: "Fullstack Javascript Immersive Program",
     institution: "Hacktiv8 Indonesia",
+    logo: "/images/education/hacktiv8.png",
     issued: "Dec 2018",
-    credential: "",
-    skills: ["React", "Tailwind", "Next.js", "Figma"],
+    credential: "https://hire.hacktiv8.com/verify/fsjs/DovanSanjaya-20181224",
+    skills: ["React", "React Native", "Express", "Javascript", "Mocha", "Chai", "Vue"],
+  },
+  {
+    name: "JavaScript Algorithms and Data Structures",
+    institution: "freeCodeCamp",
+    logo: "/images/education/freecodecamp.png",
+    issued: "Sep 2018",
+    credential: "https://freecodecamp.org/certification/dovjay/javascript-algorithms-and-data-structures",
+    skills: ["JavaScript"],
+  },
+  {
+    name: "Responsive Web Design",
+    institution: "freeCodeCamp",
+    logo: "/images/education/freecodecamp.png",
+    issued: "Jun 2018",
+    credential: "https://freecodecamp.org/certification/dovjay/responsive-web-design",
+    skills: ["CSS", "HTML"],
   },
 ]
--- src/constants/Experiences.ts
+++ src/constants/Experiences.ts
@@ -3,28 +3,171 @@ export const Experiences = [
     duration: "Januari 2024 - Present",
     company: "Nirwana Textile",
     position: "Frontend Developer | UI/UX Designer",
-    logo: "",
-    skills: ["React", "Tailwind", "Next.js", "Figma"],
+    logo: "/images/company/Logo Nirwana.png",
+    skills: ["React", "Tailwind", "Next.js", "Figma", "Remix", "TypeScript", "Tanstack Query", "Expo"],
+    projects: [
+      {
+        name: "Nirwana Textile Gateway",
+        link: "//gateway.nirwanatextile.com",
+        description: "Maintain and improve user experience for customer to buy and search for products using Nextjs 13 and Tanstack Query with Ant Design as UI library.",
+      },
+      {
+        name: "Nirwana Textile Website",
+        link: "//nirwanatextile.com",
+        description: "Design and create new landing page for better SEO and good user experience to see any available product. Build with Remix at first, but then rewrite the whole code with Next.js for better performance and maintainability.",
+      },
+      {
+        name: "Nirwana Textile Hub",
+        link: "//hub.nirwanatextile.com",
+        description: "Transitional web from social media to any existing ecosystem in Nirwana Textile. Completely rewrite the whole thing with new design and improved user experience.",
+      },
+      {
+        name: "Nirwana Textile Queue",
+        link: "//antrian.nirwanatextile.com",
+        description: "Specifically created for tracking customer order queue while also promoting and educating the customer about the products using video content from company's social media.",
+      },
+      {
+        name: "ELBE",
+        link: "https://play.google.com/store/apps/details?id=com.dovjaystudio.elbe&hl=en",
+        description: "A side project, E-LKPD Science Based on Ethnoscience is an education app for children. Made using Expo React Native."
+      },
+    ],
   },
   {
-    duration: "Januari 2024 - Present",
-    company: "Nirwana Textile",
+    duration: "Mar 2024 - Present",
+    company: "Kusogaki (Community)",
+    position: "Designer | Developer | Event Planner",
+    logo: "/images/company/kusogaki.png",
+    skills: ["Figma", "Docker", "Next.js"],
+    projects: [
+      {
+        name: "HigherLower Game",
+        link: "https://www.figma.com/design/9UhSSTpNZztm82LGlfiinq/HIgherLower?node-id=0-1&t=FCHTqQl1P75jpUkT-1",
+        description: "A game where you have to guess an anime score is higher or lower than other based on popularity or rating.",
+      },
+      {
+        name: "Anilist Wrapped (in progress)",
+        link: "",
+        description: "Wrapped your year in anilist.co and show the stats of what you've done in a year.",
+      },
+    ],
+  },
+  // {
+  //   duration: "Jun 2023 - Jun 2024",
+  //   company: "DR Sablon",
+  //   position: "Business Owner",
+  //   logo: "",
+  //   skills: [],
+  // },
+  {
+    duration: "Oct 2021 - Jan 2024",
+    company: "Freelance & Professional Development",
     position: "Frontend Developer | UI/UX Designer",
     logo: "",
-    skills: ["React", "Tailwind", "Next.js", "Figma"],
+    skills: ["Vue", "NativeScript", "Postgres", "Figma", "Vercel", "Blender", "Typescript", "Unity", "C#", "Flutter"],
+    projects: [
+      {
+        name: "Malma",
+        link: "https://www.figma.com/design/5WkICHYINvDtAUiyy34940/Malma?node-id=0-1&t=ilnH8rbrTbB8ptlm-1",
+        description: "E-commerce mainly for UMKM sellers. I make the whole design app from customer storefront to seller dashboard. Then, make the webview using NativeScript and help making the website responsive.",
+      },
+      {
+        name: "Bunny Ghost (GameJam 2022)",
+        link: "https://dovjay.itch.io/bunny-ghost",
+        description: "Participating a GameJam in 2022. It's a 1 week event where we create a game with specific theme. Build the game using Unity",
+      },
+      {
+        name: "BlueArchive Whatsapp Sticker",
+        link: "https://play.google.com/store/apps/details?id=com.dovjaystudio.bluearchive_whatsapp_stickers&hl=en",
+        description: "Fork a flutter repository for this app. Can't say I like the DX from the language.",
+      },
+    ],
   },
   {
-    duration: "Januari 2024 - Present",
-    company: "Nirwana Textile",
-    position: "Frontend Developer | UI/UX Designer",
-    logo: "",
-    skills: ["React", "Tailwind", "Next.js", "Figma"],
+    duration: "Apr 2021 - Sep 2021",
+    company: "Whitebox Indonesia",
+    position: "UI/UX Designer",
+    logo: "/images/company/whitebox.jpg",
+    skills: ["Photoshop", "Figma", "Premiere Pro"],
+    projects: [
+      {
+        name: "Manage Dribble Team Posts",
+        link: "https://dribbble.com/whitebox_id",
+        description: "Create web/app designs for company's portofolio",
+      },
+      {
+        name: "Manage Instagram Posts",
+        link: "https://www.instagram.com/bywhitebox/",
+        description: "Handle the content management, design articles, and make videos for company's social media contents.",
+      },
+      {
+        name: "Prihal Data",
+        link: "https://www.figma.com/design/Ed9doIoGL4mxgw4DLnmFef/Prihal-Data-Master?node-id=0-1&t=ey2W567ZRrYUWtQ9-1",
+        description: "Landing page design proposal for Prihal Data.",
+      },
+      {
+        name: "Sakata Utama",
+        link: "https://www.figma.com/design/5KA5BWyiXJkyaQmzT6ue9g/Sakata-Utama-Master?node-id=1-14448&t=J3WI0damGKwpF2g3-1",
+        description: "Dashboard for managing all of Sakata Utama's project, budgeting, and other company needs.",
+      },
+      {
+        name: "Ibadah.id",
+        link: "https://www.figma.com/design/YHtjWlbSpKy6UYea4A60pL/Ibadah.id-Master?node-id=0-1&t=erwtxSB06gEpNQeD-1",
+        description: "Mobile app wireframe proposal for Ibadah.id."
+      },
+    ],
   },
   {
-    duration: "Januari 2024 - Present",
-    company: "Nirwana Textile",
-    position: "Frontend Developer | UI/UX Designer",
-    logo: "",
-    skills: ["React", "Tailwind", "Next.js", "Figma"],
+    duration: "Mar 2020 - Feb 2021",
+    company: "ConceptX",
+    position: "Fullstack Developer",
+    logo: "/images/company/conceptx.png",
+    skills: ["Gatsby", "Django"],
+    projects: [
+      {
+        name: "Warehouse Stock Chain",
+        link: "",
+        description: "Create warehouse stock chain using Django.",
+      },
+      {
+        name: "ConceptX Landing Page",
+        link: "",
+        description: "Build a landing page for ConceptX.",
+      },
+      {
+        name: "Dytona Deli Krisp Website",
+        link: "",
+        description: "Build a company profile for Dytona Deli Krisp."
+      },
+    ],
+  },
+  {
+    duration: "Feb 2019 - Mar 2020",
+    company: "Red Eye Group",
+    position: "Fullstack Developer",
+    logo: "/images/company/redeyegroup.jpeg",
+    skills: ["JavaScript", "React", "React Native", "MongoDB", "Fastify", "Express", "Python", "Django"],
+    projects: [
+      {
+        name: "Upvolv E-commerce",
+        link: "https://zpl.io/aBzLkpQ",
+        description: "Build an e-commerce for gamers that sells digital products. Made using React, and Fastify.",
+      },
+      {
+        name: "Upvolv Media",
+        link: "https://zpl.io/VOednPY",
+        description: "Build a media platform for publication and increase SEO for Upvolv.",
+      },
+      {
+        name: "Sprintholic",
+        link: "",
+        description: "Company's sprint tracker build with React Native."
+      },
+      {
+        name: "Mofoto",
+        link: "",
+        description: "Photographer application to manage photos that's been taken from a visitor to a place/event and sell it to them. Made using Django."
+      },
+    ],
   },
 ]
